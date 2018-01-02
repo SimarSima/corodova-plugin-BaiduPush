567f67f0159657c09818db635c614fb489e7fc63
Simar <441315491@qq.com>
2018-01-02 09:46:55 +0800

    Update

--- www/BaiduPushCordovaPlugin.js
+++ www/BaiduPushCordovaPlugin.js
@@ -1,8 +1,8 @@
 var exec = require('cordova/exec');
 
 var BaiduPushCordovaPlugin = {
-    startWork: function (arg0, success, error) {
-        exec(success, error, "BaiduPushCordovaPlugin", "startWork", [arg0]);
+    startWork: function (success, error) {
+        exec(success, error, "BaiduPushCordovaPlugin", "startWork", []);
     }
 }
 module.exports = BaiduPushCordovaPlugin;
